f21564d9d28287186ff15a58e2aef6d70b2d418d
Benjamin Bolte <ben@bolte.cc>
2023-03-08 18:46:25 -0500

    run script

--- spot_micro_pi/run.py
+++ spot_micro_pi/run.py
@@ -8,13 +8,10 @@ def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("servo_id", type=int, help="The servo ID to control")
     parser.add_argument("angle", type=float, help="The angle to move the servo to")
-    parser.add_argument("-w", "--wait", type=float, default=1.0, help="How long to wait after running")
     args = parser.parse_args()
 
     servo_set = ServoSet(servo_ids=[args.servo_id])
-    for _ in range(10):
-        servo_set.set_angle(args.servo_id, args.angle)
-        time.sleep(args.wait / 10)
+    servo_set.set_angle(args.servo_id, args.angle)
 
 
 if __name__ == "__main__":
